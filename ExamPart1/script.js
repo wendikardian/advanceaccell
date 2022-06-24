619a67d1266ef29df6fb7055126cf77fa2669924
wendikardian <wendikardian@gmail.com>
2022-06-25 00:01:05 +0700

    ..

---
+++ ExamPart1/script.js
@@ -0,0 +1,27 @@
+function changeImage(element){
+    element.setAttribute('src', 'img/img-header2.png')
+}
+function changeImageBack(element){
+    element.setAttribute('src', 'img/img-header1.png')
+}
+
+
+var btnLike = document.querySelector('#green')
+var btnDislike = document.querySelector("#red")
+
+btnLike.onclick = likeColor()
+btnDislike.onclick = dislikeColor()
+
+function likeColor(){
+    if(btnDislike.classList.contains('red')){
+        btnDislike.classList.remove('red')
+    }
+    this.classList.toggle('green')
+}
+
+function dislikeColor(){
+    if(btnLike.classList.contains('green')){
+        btnLike.classList.remove('green')
+    }
+    this.classList.toggle('red')
+}
