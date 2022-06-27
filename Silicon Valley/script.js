b99efeee39938ba579e45a7372fece81ec54f0d7
wendikardian <wendikardian@gmail.com>
2022-06-28 00:07:25 +0700

    hoho

--- Silicon Valley/script.js
+++ Silicon Valley/script.js
@@ -1,13 +1,14 @@
 window.addEventListener('scroll', reveal);
 
 function reveal(){
-    var reveals = document.quearySelectorAll('.reveal');
+    var reveals = document.querySelectorAll('.reveal');
+    console.log(reveals)
     reveals.forEach(reveal => {
         var windowHeight = window.innerHeight;
         var revealTop = reveal.getBoundingClientRect().top;
         var revealPoint = 40;
         if(revealTop < windowHeight - revealPoint) {
-            reveal.ClassList.add('active');
+            reveal.classList.add('active');
         }else{
             reveal.classList.remove('active');
         }
