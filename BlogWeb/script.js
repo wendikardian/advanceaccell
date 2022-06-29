b2bc4b74b5fb9a963d8a8b25f2007a2e20af297b
wendikardian <wendikardian@gmail.com>
2022-06-29 11:29:56 +0700

    hoho:)

--- BlogWeb/script.js
+++ BlogWeb/script.js
@@ -145,3 +145,29 @@ if(btnSubmit){
         next.setAttribute('value', 'https://webblogadvance.netlify.app/contact.html')
     })
 }
+
+var splash = document.querySelector(".splash")
+if(splash){
+    document.addEventListener('DOMContentLoaded', function(event){
+        setTimeout(function() {
+            splash.style.display = "none"
+        }, 3000)
+    })
+}
+
+var animate = document.querySelector('.astronaut-takeoff')
+if(animate){
+    document.addEventListener('DOMContentLoaded', function(event){
+        setTimeout(function() {
+            animate.classList.add('animation')
+        }, 400)
+    })
+}
+
+var splashText = document.querySelector('.splash-text')
+
+if(splashText){
+    setTimeout(function() {
+        splashText.innerHTML = "Come in !"
+    }, 2000)
+}
